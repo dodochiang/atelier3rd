0f8a5a6e25fa240f6ad4765520161a6b526d5852
Dodo Chiang <dodochiang@gmail.com>
2025-06-30 11:23:46 +0800

    d

--- admin/netlify-cms.js
+++ admin/netlify-cms.js
@@ -1,2 +1,1 @@
-/* Netlify CMS v2.15.72 local build */
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports,require("react"),require("react-dom")):"function"==typeof define&&define.amd?define(["exports","react","react-dom"],t):t((e="undefined"!=typeof globalThis?globalThis:e||self).window=e.window||{},e.React,e.ReactDOM)}(...core code omitted...)
+window.CMS = { init: function(config) { console.log("Netlify CMS mock loaded", config); } };
